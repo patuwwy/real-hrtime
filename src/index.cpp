c84a297eaa2cf5339d3c07b519e3f30d11bb656f
patuwwy <patuwwy@gmail.com>
2021-10-17 01:43:51 +0200

    Linux specified build

--- src/index.cpp
+++ src/index.cpp
@@ -5,21 +5,14 @@
 #include <time.h>
 #include <napi.h>
 #include <stdio.h>
-
-int64_t rht() {
-    struct timespec ts;
-
-    clock_gettime(CLOCK_REALTIME, &ts);
-
-    return ts.tv_sec * 1e9 + ts.tv_nsec;
-}
+#include <rht-linux.h>
 
 Napi::BigInt bigint(const Napi::CallbackInfo& info) {
-    return Napi::BigInt::New(info.Env(), rht());
+    return Napi::BigInt::New(info.Env(), _bigint());
 }
 
-Napi::String str(const Napi::CallbackInfo& info) {
-    return Napi::String::New(info.Env(), std::to_string(rht()));
+Napi::String stringified(const Napi::CallbackInfo& info) {
+    return Napi::String::New(info.Env(), _stringified());
 }
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
@@ -30,7 +23,7 @@ Napi::Object Init(Napi::Env env, Napi::Object exports) {
 
     exports.Set(
         Napi::String::New(env, "stringified"),
-        Napi::Function::New(env, str)
+        Napi::Function::New(env, stringified)
     );
 
     return exports;
